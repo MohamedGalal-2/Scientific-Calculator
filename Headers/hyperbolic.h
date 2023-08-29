6f2b941dc6707c8432253e0e9b694c885a94cd19
Mohamed Galal <101941093+MohamedGalal-2@users.noreply.github.com>
2023-08-29 22:40:53 +0300

    Delete Headers directory

--- Headers/hyperbolic.h
+++
@@ -1,7 +0,0 @@
-/*Includes Section*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-/*Functions Decleration Section*/
-float Hyperbolic(int, float);
--- Headers/logarithms.h
+++
@@ -1,9 +0,0 @@
-/*Includes Section*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-/*Functions Decleration Section*/
-float Log(float, float);
-float LN(float);
-float Exponential(float);
