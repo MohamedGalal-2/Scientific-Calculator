a5b52eb226994e9568112a20365a69d62b3cfd3e
Mohamed Galal <101941093+MohamedGalal-2@users.noreply.github.com>
2023-08-29 23:04:58 +0300

    Add files via upload

---
+++ trigfunctions.h
@@ -0,0 +1,11 @@
+/*Includes Section*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*Macros Definition Section*/
+#define M_PI (3.14159265358979323846) /*PI Value*/
+
+/*Function Decleration Section*/
+float Rad(float);
+float Trig(unsigned short int, unsigned short int, float);
